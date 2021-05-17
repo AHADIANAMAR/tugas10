0bc54a743120d3c8edbbcf7fe6f1890d47237437
AHADIANAMAR <79738225+AHADIANAMAR@users.noreply.github.com>
2021-05-17 10:45:12 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php 
+$host       = "localhost";
+$user       = "root";
+$password   = "";
+$database   = "pertemuan10"; //nama database
+$koneksi    = mysqli_connect($host, $user, $password, $database);
+?>
